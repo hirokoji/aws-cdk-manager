4119702137f316d28b60c34d4599111c37ae593e
Hiro Kojima <guitarsaiko@gmail.com>
2020-07-29 11:08:43 +0900

    Add cli interface

--- src/Cli.ts
+++ src/Cli.ts
@@ -1,14 +1,33 @@
 import { program } from 'commander';
 
 export class Cli {
-    constructor() {}
-
     public cli = async (argv: any): Promise<void> => {
         program
             .command('list')
-            .description('list greengrass group')
-            .action(async cmd => {
-                console.log('will list greengrass here');
+            .description('list managed aws-cdk stack')
+            .action(async () => {
+                console.log('Comming Soon. This function has not implemented yet.');
+            });
+
+        program
+            .command('add')
+            .description('add aws-cdk stack to manage by this tool')
+            .action(async () => {
+                console.log('Comming Soon. This function has not implemented yet.');
+            });
+
+        program
+            .command('rm')
+            .description('remove aws-cdk stack from managing list')
+            .action(async () => {
+                console.log('Comming Soon. This function has not implemented yet.');
+            });
+
+        program
+            .command('logs')
+            .description('remove aws-cdk stack from managing list')
+            .action(async () => {
+                console.log('Comming Soon. This function has not implemented yet.');
             });
 
         program.parse(argv);
